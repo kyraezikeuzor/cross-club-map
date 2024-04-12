605003de10d153b93a36c98c1f86710631df2f90
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2024-04-11 20:11:39 -0500

    Added Austin HS, Village School, 7 Lakes HS

--- data/clubs.ts
+++ data/clubs.ts
@@ -13,5 +13,17 @@ export const clubs: clubType[] = [
     {
         "school":"George Ranch Memorial",
         "geocode": [29.51657823925303, -95.69124451979916]
+    },
+    {
+        "school":"Stephen F. Austin High School",
+        "geocode":[29.668655280073885, -95.67452778826124]
+    },
+    {
+        "school":"The Village School",
+        "geocode":[29.746676407928852, -95.61949697505969]
+    },
+    {
+        "school":"Seven Lakes High School",
+        "geocode":[29.707747914141724, -95.80817706581954]
     }
 ];
